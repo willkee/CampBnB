f337b9d1afc6c511a3ce660fc8d07af145d3c911
Will Kee <william.w.kee@gmail.com>
2022-04-12 16:48:28 -0600

    added db tables and seed files for bookings and spots

---
+++ backend/db/migrations/20220412194822-create-spot.js
@@ -0,0 +1,72 @@
+"use strict";
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("Spots", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			ownerId: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+				references: { model: "Users" },
+			},
+			name: {
+				allowNull: false,
+				type: Sequelize.STRING(255),
+			},
+			address: {
+				type: Sequelize.STRING(255),
+			},
+			city: {
+				allowNull: false,
+				type: Sequelize.STRING(255),
+			},
+			lat: {
+				type: Sequelize.DECIMAL(10, 6),
+			},
+			long: {
+				type: Sequelize.DECIMAL(10, 6),
+			},
+			imageUrl: {
+				allowNull: false,
+				type: Sequelize.STRING(2048),
+			},
+			type: {
+				allowNull: false,
+				type: Sequelize.STRING(50),
+			},
+			price: {
+				allowNull: false,
+				type: Sequelize.DECIMAL(10, 2),
+			},
+			description: {
+				type: Sequelize.TEXT,
+			},
+			capacity: {
+				allowNull: false,
+				type: Sequelize.INTEGER,
+			},
+			open: {
+				allowNull: false,
+				type: Sequelize.BOOLEAN,
+				defaultValue: true,
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: new Date("2021-12-01"),
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: new Date("2021-12-01"),
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("Spots");
+	},
+};
---
+++ backend/db/models/spot.js
@@ -0,0 +1,109 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+	class Spot extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+			Spot.belongsTo(models.User, { foreignKey: "ownerId" });
+			Spot.hasMany(models.Booking, { foreignKey: "spotId" });
+		}
+	}
+	Spot.init(
+		{
+			ownerId: {
+				allowNull: false,
+				type: DataTypes.INTEGER,
+			},
+			name: {
+				allowNull: false,
+				type: DataTypes.STRING,
+				validate: {
+					len: [1, 255],
+				},
+			},
+			address: {
+				type: DataTypes.STRING,
+				validate: {
+					max: 255,
+				},
+			},
+			city: {
+				allowNull: false,
+				type: DataTypes.STRING,
+				validate: {
+					len: [1, 255],
+				},
+			},
+			lat: {
+				type: DataTypes.DECIMAL,
+				validate: {
+					max: {
+						args: 41.0,
+						msg: "Please enter a valid latitude within the State of Colorado",
+					},
+					min: {
+						args: 37.0,
+						msg: "Please enter a valid latitude within the State of Colorado",
+					},
+				},
+			},
+			long: {
+				type: DataTypes.DECIMAL,
+				validate: {
+					max: {
+						args: -102.0,
+						msg: "Please enter a valid longitude within the State of Colorado",
+					},
+					min: {
+						args: -109.0,
+						msg: "Please enter a valid longitude within the State of Colorado",
+					},
+				},
+			},
+			imageUrl: {
+				allowNull: false,
+				type: DataTypes.STRING,
+				validate: {
+					len: [3, 2048],
+				},
+			},
+			type: {
+				allowNull: false,
+				type: DataTypes.STRING,
+				validate: {
+					len: [1, 60],
+				},
+			},
+			price: {
+				allowNull: false,
+				type: DataTypes.DECIMAL,
+				validate: {
+					min: 0,
+					max: 99999999.99,
+				},
+			},
+			description: {
+				type: DataTypes.TEXT,
+			},
+			capacity: {
+				allowNull: false,
+				type: DataTypes.INTEGER,
+			},
+			open: {
+				allowNull: false,
+				type: DataTypes.BOOLEAN,
+				defaultValue: true,
+			},
+		},
+		{
+			sequelize,
+			modelName: "Spot",
+		}
+	);
+	return Spot;
+};
