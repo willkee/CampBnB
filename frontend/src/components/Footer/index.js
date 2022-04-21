796b8a8e6d005a185a4811cc6806040aca6fc12a
Will Kee <william.w.kee@gmail.com>
2022-04-21 10:28:26 -0600

    added dev icons to footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,21 +3,43 @@ import styles from "./Footer.module.css";
 const Footer = () => {
 	return (
 		<div className={styles.footer_container}>
-			<div>Developed by Will Kee</div>
-			<a
-				href="https://github.com/willkee"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<i className="fa-brands fa-github-square" />
-			</a>
-			<a
-				href="https://www.linkedin.com/in/will-kee/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<i className="fa-brands fa-linkedin" />
-			</a>
+			<div className={styles.left}>
+				<i id={styles.icon1} className="devicon-vscode-plain" />
+				<i id={styles.icon2} className="devicon-javascript-plain" />
+				<i id={styles.icon3} className="devicon-nodejs-plain" />
+				<i id={styles.icon4} className="devicon-react-original" />
+				<i id={styles.icon5} className="devicon-redux-original" />
+				<i id={styles.icon6} className="devicon-html5-plain" />
+				<i id={styles.icon7} className="devicon-css3-plain" />
+				<i id={styles.icon8} className="devicon-postgresql-plain" />
+				<i id={styles.icon9} className="devicon-express-original" />
+				<i id={styles.icon10} className="devicon-sequelize-plain" />
+				<i id={styles.icon11} className="devicon-heroku-plain" />
+				<i id={styles.icon12} className="devicon-canva-original" />
+				<i id={styles.icon13} className="devicon-figma-plain" />
+				<i id={styles.icon14} className="devicon-google-plain" />
+				<i
+					id={styles.icon15}
+					className="devicon-amazonwebservices-original"
+				/>
+			</div>
+			<div className={styles.right}>
+				<div>Developed by Will Kee</div>
+				<a
+					href="https://github.com/willkee"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i className="fa-brands fa-github-square" />
+				</a>
+				<a
+					href="https://www.linkedin.com/in/will-kee/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<i className="fa-brands fa-linkedin" />
+				</a>
+			</div>
 		</div>
 	);
 };
