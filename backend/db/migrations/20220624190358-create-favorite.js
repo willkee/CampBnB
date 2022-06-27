5af22387df3a5f9b27da900785d09ee1a9499a96
Will Kee <william.w.kee@gmail.com>
2022-06-27 16:39:32 -0600

    favorite db model created

---
+++ backend/db/migrations/20220624190358-create-favorite.js
@@ -0,0 +1,32 @@
+"use strict";
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("Favorites", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			spotId: {
+				type: Sequelize.INTEGER,
+				references: { model: "Spots" },
+			},
+			userId: {
+				type: Sequelize.INTEGER,
+				references: { model: "Users" },
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("Favorites");
+	},
+};
