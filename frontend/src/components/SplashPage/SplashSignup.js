2a22afc70ba6e2918df5ebcb860ae6bdc147226b
Will Kee <will.kee.dev@gmail.com>
2024-05-14 10:52:27 -0600

    fully responsive splash screen, log in and sign up

---
+++ frontend/src/components/SplashPage/SplashSignup.js
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+
+import styles from "./SplashPage.module.css";
+const SplashSignup = ({ active }) => {
+	const dispatch = useDispatch();
+	const location = useLocation();
+	const navigate = useNavigate();
+
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPassword, setConfirmPassword] = useState("");
+	const [errors, setErrors] = useState([]);
+
+	const demoLogin = async (e) => {
+		await dispatch(sessionActions.login("demo@user.io", "password"));
+		await dispatch(sessionActions.getMyBookings());
+		if (location.pathname === "/") return navigate("/main");
+		return;
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setErrors([]);
+
+		try {
+			await dispatch(
+				sessionActions.signup({
+					firstName: firstName.trim(),
+					lastName: lastName.trim(),
+					email: email.trim(),
+					password,
+					confirmPassword,
+				})
+			);
+			await dispatch(sessionActions.getMyBookings());
+			if (location.pathname === "/") {
+				return navigate("/main");
+			}
+			return;
+		} catch (res) {
+			const data = await res.json();
+			if (data && data.errors)
+				setErrors(
+					data.errors.filter((error) => error !== "Invalid value")
+				);
+			return;
+		}
+	};
+
+	return (
+		<form
+			className={`${styles.signup_form} ${
+				active ? styles.visible : styles.invisible
+			}`}
+			onSubmit={handleSubmit}
+		>
+			<h1>Sign up</h1>
+			{errors.length > 0 && (
+				<div className={styles.form_error}>
+					{errors.map((error, idx) => (
+						<div key={idx}>{error}</div>
+					))}
+				</div>
+			)}
+			<label>First Name</label>
+			<input
+				className={styles.input}
+				type="text"
+				value={firstName}
+				onChange={(e) => setFirstName(e.target.value)}
+				required
+			/>
+			<label>Last Name</label>
+			<input
+				className={styles.input}
+				type="text"
+				value={lastName}
+				onChange={(e) => setLastName(e.target.value)}
+				required
+			/>
+			<label>Email</label>
+			<input
+				className={styles.input}
+				type="text"
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+				required
+			/>
+			<label>Password</label>
+			<input
+				className={styles.input}
+				type="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+				autoComplete="none"
+				required
+			/>
+			<label>Confirm Password</label>
+			<input
+				className={styles.input}
+				type="password"
+				value={confirmPassword}
+				onChange={(e) => setConfirmPassword(e.target.value)}
+				autoComplete="none"
+				required
+			/>
+			<button className={styles.signup_button} type="submit">
+				Sign Up
+			</button>
+			<button
+				className={styles.demo}
+				onClick={() => demoLogin()}
+				type="button"
+			>
+				Demo User
+			</button>
+		</form>
+	);
+};
+
+export default SplashSignup;
--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,110 +1,64 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import SplashLogin from "./SplashLogin";
+import SplashSignup from "./SplashSignup";
+import styles from "./SplashPage.module.css";
+
 // import { useDispatch } from "react-redux";
 // import * as sessionActions from "../../store/session";
 
 // import { currentModal, showModal } from "../../store/modal";
 // import LoginForm from "../LoginForm";
 // import SignUpForm from "../SignUpForm";
-import styles from "./SplashPage.module.css";
-
-import SplashLogin from "./SplashLogin";
 
 const SplashPage = ({ sessionUser }) => {
+	const [activeForm, setActiveForm] = useState("signin");
+	const [switchMessage, setSwitchMessage] = useState("No account? Sign up.");
 	const navigate = useNavigate();
-	// const displaySignupForm = () => {
-	// 	dispatch(currentModal(SignUpForm));
-	// 	dispatch(showModal());
-	// };
 
-	// const displayLoginForm = () => {
-	// 	dispatch(currentModal(LoginForm));
-	// 	dispatch(showModal());
-	// };
+	const content = useRef(null);
+	const [sz, setSz] = useState({ height: 0, width: 0 });
 
 	useEffect(() => {
-		if (sessionUser) {
-			navigate("/main");
+		setSz(content.current.getBoundingClientRect());
+	}, [activeForm]);
+
+	// useEffect(() => {
+	// 	if (sessionUser) navigate("/main");
+	// }, [sessionUser, navigate]);
+
+	const handleSwitch = () => {
+		if (activeForm === "signin") {
+			setActiveForm("signup");
+			setSwitchMessage("Have an account? Sign in.");
+		} else {
+			setActiveForm("signin");
+			setSwitchMessage("No account? Sign up.");
 		}
-	}, [sessionUser, navigate]);
+	};
 
 	return (
 		<div className={styles.container}>
-			<div className={styles.main}>
-				<header>
-					<img
-						src={`${process.env.PUBLIC_URL}/images/logo_only.png`}
-						alt="green logo"
-					/>
-					<p>campbnb</p>
-				</header>
-				<SplashLogin />
-			</div>
-			{/* <div className={styles.splash_content}>
-				<img
-					src={`${process.env.PUBLIC_URL}/images/logo_only.png`}
-					alt="green logo"
-				/>
-				<h1>Welcome to CampBnB</h1>
-				<p>Escape and discover your next outdoor adventure.</p>
-				<p>
-					CampBnB is a site where you can find and discover new places
-					to camp within the state of Colorado. If you have a place to
-					share, you can list your spot too.
-				</p>
-				<div className={styles.buttons}>
-					{sessionUser ? (
-						<>
-							<div>{`Welcome back ${sessionUser.firstName}!`}</div>
-							<div
-								className={styles.just_browsing}
-								onClick={() => navigate("/main")}
-							>
-								<div>Enter Here</div>
-							</div>
-						</>
-					) : (
-						<div className={styles.splash_button_container}>
-							<div className={styles.signup_login}>
-								<div className={styles.new_user}>
-									Joining us for the first time?
-									<div
-										role="button"
-										onClick={displaySignupForm}
-									>
-										Sign Up
-									</div>
-								</div>
-								<div className={styles.demo_user}>
-									Demo the Site
-									<div role="button" onClick={demoLogin}>
-										Demo User
-									</div>
-								</div>
-								<div className={styles.existing_user}>
-									Returning user?
-									<div
-										role="button"
-										onClick={displayLoginForm}
-									>
-										Log In
-									</div>
-								</div>
-							</div>
-							<div>
-								Just browsing? Explore the site{" "}
-								<span
-									id={styles.here}
-									onClick={() => navigate("/main")}
-								>
-									here
-								</span>
-								.
-							</div>
-						</div>
-					)}
+			<div className={styles.main} style={{ height: `${sz.height}px` }}>
+				<div className={styles.content} ref={content}>
+					<header>
+						<img
+							src={`${process.env.PUBLIC_URL}/images/logo_only.png`}
+							alt="green logo"
+						/>
+						<p>campbnb</p>
+					</header>
+					<SplashLogin active={activeForm === "signin"} />
+					<SplashSignup active={activeForm === "signup"} />
+					<button
+						className={styles.switch_button}
+						type="button"
+						onClick={handleSwitch}
+					>
+						{switchMessage}
+					</button>
 				</div>
-			</div> */}
+			</div>
 		</div>
 	);
 };
